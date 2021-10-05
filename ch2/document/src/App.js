cfc61f1e7eb95620dfb64e89b843a67f6caddc60
manbalboy <manbalboy@hanmail.net>
2021-10-06 00:35:51 +0900

    라이프사이클

--- ch2/document/src/App.js
+++ ch2/document/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 // import Composition from "./components/2-4-props/Composition";
 // import Extraction from "./components/2-4-props/Extraction/Extraction";
-import ClassComponent from "./components/2-5-state/ClassComponent";
+
+// import ClassComponent from "./components/2-5-state/ClassComponent";
 import FunctionalComponent from "./components/2-5-state/FunctionalComponent";
+import ClassComponent from "./components/2-6-lifeCycle/ClassComponent";
 
 function App() {
   return (
     <div className="App">
       <ClassComponent />
-      <FunctionalComponent />
+      {/*<FunctionalComponent />*/}
       {/*<Composition />*/}
       {/*<Extraction />*/}
       {/*<Composition />*/}
---
+++ ch2/document/src/components/2-6-lifeCycle/ClassComponent.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor : ");
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerID = setInterval(() => this.tick(), 10000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
